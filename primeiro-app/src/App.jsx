65be2b8f10fad5ea5453d71fcc83582c69ae2225
Rodrigo Bossini <professorbossini@gmail.com>
2025-03-31 20:15:13 -0300

    feat(ap03-react): encerra a apostila

--- primeiro-app/src/App.jsx
+++ primeiro-app/src/App.jsx
@@ -1,14 +1,25 @@
+import './styles.css'
+
 const App = () => {
-  //kebab case
-  // const background-color-outra-palavra;
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', borderRadius: 8, width: '100%', color: 'white', cursor: 'pointer', borderStyle: 'none'}
+
+  const textoDoRotulo = 'Nome:'
+
+  const textoBotao = () => 'Enviar'
+
+  const onBotaoClicado = () => alert('ok, clicou')
+
   return (
     <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
-      <label htmlFor='nome' style={{display: 'block', marginBottom: 4}}>
-        Nome:
+      <label className='rotulo' htmlFor='nome' style={{display: 'block', marginBottom: 4}}>
+        {textoDoRotulo}
       </label>
       <input type='text' style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', outline: 'none', width: '100%', borderRadius: 8}}/>
-      <button type='button' style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', borderRadius: 8, width: '100%', color: 'white', cursor: 'pointer', borderStyle: 'none'}}>
-        Enviar
+      <button 
+        type='button'
+        style={estilosBotao}
+        onClick={() => onBotaoClicado()}>
+        {textoBotao()}
       </button>
     </div>
   )
