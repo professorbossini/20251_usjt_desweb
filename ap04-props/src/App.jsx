b102e199bbbd104a3f2aa5decc9561f43e02dc00
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-07 22:03:27 -0300

    feat(ap04-react): lida com o layout responsivo

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -1,6 +1,31 @@
 const App = () => {
   return (
-    <div>Oi</div>
+    <div className="container border border-dark">
+      <div className="row">
+        <div className="col-12">
+          <div>
+            <p>Meu hipo</p>
+            <i className="fa-solid fa-hippo"></i>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 col-lg-3 col-md-6">
+            <p className="border border-dark">Pedido 1</p>
+          </div>
+          <div className="col-sm-12 col-lg-3 col-md-6">
+            <p className="border border-dark">Pedido 2</p>
+          </div>
+          <div className="col-sm-12 col-lg-3 col-md-6">
+            <p className="border border-dark">Pedido 3</p>
+          </div>
+          <div className="col-sm-12 col-lg-3 col-md-6">
+            <p className="border border-dark">Pedido 4</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
