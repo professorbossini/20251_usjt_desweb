0a35b4c76816d7c3dd1e127d351f748351a06a8a
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-14 20:19:58 -0300

    feat(ap04-react): cria lista de 4 pedidos

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -5,21 +5,89 @@ const App = () => {
         <div className="col-12">
           <div>
             <p>Meu hipo</p>
-            <i className="fa-solid fa-hippo"></i>
+            <i className="fa-solid fa-hippo fa-3x"></i>
           </div>
         </div>
         <div className="row">
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <p className="border border-dark">Pedido 1</p>
+            <div className="card">
+              <div className="card-header">
+                22/04/2024
+              </div>
+              <div className="card-body d-flex">
+                <div className="d-flex align-items-center">
+                  <i className="fa-solid fa-mug-hot fa-2x"></i>
+                </div>
+                <div className="ms-3 border border-danger flex-grow-1">
+                  <h4 className="text-center">
+                    Café quente
+                  </h4>
+                  <p className="text-center">
+                    Café expresso com açúcar. R$: 7,90
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <p className="border border-dark">Pedido 2</p>
+            <div className="card">
+              <div className="card-header">
+                22/04/2024
+              </div>
+              <div className="card-body d-flex">
+                <div className="d-flex align-items-center">
+                  <i className="fa-solid fa-cookie fa-2x"></i>
+                </div>
+                <div className="ms-3 border border-danger flex-grow-1">
+                  <h4 className="text-center">
+                    Biscoito
+                  </h4>
+                  <p className="text-center">
+                    Cookie suculento
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <p className="border border-dark">Pedido 3</p>
+            <div className="card">
+              <div className="card-header">
+                22/04/2024
+              </div>
+              <div className="card-body d-flex">
+                <div className="d-flex align-items-center">
+                  <i className="fa-solid fa-car fa-2x"></i>
+                </div>
+                <div className="ms-3 border border-danger flex-grow-1">
+                  <h4 className="text-center">
+                    Mobi
+                  </h4>
+                  <p className="text-center">
+                    Carro pequeno e sem espaço
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <p className="border border-dark">Pedido 4</p>
+            <div className="card">
+              <div className="card-header">
+                22/04/2024
+              </div>
+              <div className="card-body d-flex">
+                <div className="d-flex align-items-center">
+                  <i className="fa-solid fa-bicycle fa-2x"></i>
+                </div>
+                <div className="ms-3 border border-danger flex-grow-1">
+                  <h4 className="text-center">
+                    Bicicleta
+                  </h4>
+                  <p className="text-center">
+                    Bicicleta aro 16
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
