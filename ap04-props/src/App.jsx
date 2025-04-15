8242cb41b40714d5fc60d1b7eab42ab218df115b
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-14 21:44:06 -0300

    refactor(ap04-react): refatora componente pedido
    - ilustramos o uso do props
    - retomar fazendo o componente de feedback
    - agora o hipo tem esposa

--- ap04-props/src/App.jsx
+++ ap04-props/src/App.jsx
@@ -1,93 +1,44 @@
+import Pedido from "./Pedido"
+
 const App = () => {
   return (
     <div className="container border border-dark">
       <div className="row">
         <div className="col-12">
           <div>
-            <p>Meu hipo</p>
+            <p>Meus hipos</p>
             <i className="fa-solid fa-hippo fa-3x"></i>
+            <i className="ms-1 fa-solid fa-hippo fa-3x fa-flip-horizontal"></i>
           </div>
         </div>
         <div className="row">
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <div className="card">
-              <div className="card-header">
-                22/04/2024
-              </div>
-              <div className="card-body d-flex">
-                <div className="d-flex align-items-center">
-                  <i className="fa-solid fa-mug-hot fa-2x"></i>
-                </div>
-                <div className="ms-3 border border-danger flex-grow-1">
-                  <h4 className="text-center">
-                    Café quente
-                  </h4>
-                  <p className="text-center">
-                    Café expresso com açúcar. R$: 7,90
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Pedido 
+              data="22/04/2024"
+              titulo="Café quente"
+              icone="mug-hot"
+              descricao="Café quente expresso no copo americano"/>
           </div>
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <div className="card">
-              <div className="card-header">
-                22/04/2024
-              </div>
-              <div className="card-body d-flex">
-                <div className="d-flex align-items-center">
-                  <i className="fa-solid fa-cookie fa-2x"></i>
-                </div>
-                <div className="ms-3 border border-danger flex-grow-1">
-                  <h4 className="text-center">
-                    Biscoito
-                  </h4>
-                  <p className="text-center">
-                    Cookie suculento
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Pedido 
+              data="23/04/2024"
+              titulo="Biscoito"
+              icone="cookie"
+              descricao="Cookie suculento"/>
           </div>
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <div className="card">
-              <div className="card-header">
-                22/04/2024
-              </div>
-              <div className="card-body d-flex">
-                <div className="d-flex align-items-center">
-                  <i className="fa-solid fa-car fa-2x"></i>
-                </div>
-                <div className="ms-3 border border-danger flex-grow-1">
-                  <h4 className="text-center">
-                    Mobi
-                  </h4>
-                  <p className="text-center">
-                    Carro pequeno e sem espaço
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Pedido 
+              data="14/04/2025"
+              titulo="Mobi"
+              icone="car"
+              descricao="Carro pequeno e sem espaço"/>
           </div>
           <div className="col-sm-12 col-lg-3 col-md-6">
-            <div className="card">
-              <div className="card-header">
-                22/04/2024
-              </div>
-              <div className="card-body d-flex">
-                <div className="d-flex align-items-center">
-                  <i className="fa-solid fa-bicycle fa-2x"></i>
-                </div>
-                <div className="ms-3 border border-danger flex-grow-1">
-                  <h4 className="text-center">
-                    Bicicleta
-                  </h4>
-                  <p className="text-center">
-                    Bicicleta aro 16
-                  </p>
-                </div>
-              </div>
-            </div>
+          <Pedido 
+              data="17/03/2024"
+              titulo="Bicicleta"
+              icone="bicycle"
+              descricao="Bicicleta aro 16 cor amarela"/>
           </div>
         </div>
 
---
+++ ap04-props/src/Pedido.jsx
@@ -0,0 +1,27 @@
+// rafce
+import React from 'react'
+
+const Pedido = (props) => {
+  return (
+    <div className="card">
+      <div className="card-header">
+        {props.data}
+      </div>
+      <div className="card-body d-flex">
+        <div className="d-flex align-items-center">
+          <i className={`fa-solid fa-${props.icone} fa-2x`}></i>
+        </div>
+        <div className="ms-3 border border-danger flex-grow-1">
+          <h4 className="text-center">
+            {props.titulo}
+          </h4>
+          <p className="text-center">
+            {props.descricao}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Pedido
